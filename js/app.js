8c441b7e5ae9e8b2f374196e4177038b2eb9a529
hrc099 <hrc099@gmail.com>
2017-10-08 12:27:02 +0200

    Edits on homepage and work gallery

--- js/app.js
+++ js/app.js
@@ -17,18 +17,11 @@ if (htmlHeight < screenHeight) {
     document.getElementById('footer').style.marginTop = screenHeight + 40 - htmlHeight + 'px';
 }
 
-function zoom(img) {
-    
-}
-
 // Timeout function, activate animation on each image with a delay
 // Makes images load one by one
 function timeout(i) {
     setTimeout(function() {
         images[i].classList.add('load-in');
-        images[i].addEventListener('click', function() {
-            
-        });
     }, 100*i);
 }
 
@@ -40,3 +33,48 @@ for(i = 0; i < images.length; i++) {
     timeout(i);
 }
 
+// Lightbox options for img on product gallery page
+lightbox.option({
+    'albumLabel': 'Rad %1 od %2',
+    'fadeDuration': 400,
+    'resizeDuration': 500,
+    'wrapAround': true
+})
+
+// Smooth scroll
+// Select all links with hashes
+$('a[href*="#"]')
+// Remove links that don't actually link to anything
+.not('[href="#"]')
+.not('[href="#0"]')
+.click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+  ) {
+    // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 500, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        //$target.focus();
+        if ($target.is(":focus")) { // Checking if the target was focused
+          return false;
+        } else {
+          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+          //$target.focus(); // Set focus again
+        };
+      });
+    }
+  }
+});
